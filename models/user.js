5a6049163d36f59f71dfdc14e1aac949b72c2a12
unknown <miguelmoreno142@yahoo.com>
2023-06-07 22:25:56 -0400

    edit code in User.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 
-class user extends Model {
+class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-user.init(
+User.init(
   {
     id: {
         type: DataTypes.INTEGER,
