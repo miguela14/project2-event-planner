fdb93f0b2b348e3956f54c33a9a5327bf82dd4fc
RandyYanish <rtaylorfargo@gmail.com>
2023-06-08 16:47:21 -0700

    models update
    updated models to include code for Registration and updated the index to show those changes and relationships

--- models/index.js
+++ models/index.js
@@ -1,13 +1,40 @@
 const User = require('./User');
 const Event = require('./Event');
-// const Registration = require('./Registration')
+const Registration = require('./Registration');
 
 User.hasMany(Event, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Event.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Event,};
+User.belongsTo(Event, {
+    through: Registration,
+    foreignKey: 'user_id',
+});
+
+Event.belongsToMany(User, {
+    through: Registration,
+    foreignKey: 'event_id',
+});
+
+Registration.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Registration.belongsTo(Event, {
+    foreignKey: 'event_id',
+});
+
+User.hasMany(Registration, {
+    foreignKey: 'event_id',
+});
+
+Event.hasMany(Registration, {
+    foreignKey: 'event_id',
+});
+
+module.exports = { User, Event, Registration };
