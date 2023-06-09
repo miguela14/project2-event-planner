2dc08b2a551425c2513477fed8d2fddf17344555
RandyYanish <rtaylorfargo@gmail.com>
2023-06-08 18:41:05 -0700

    models v2.0.2
    refactored models and updated server.js

--- models/index.js
+++ models/index.js
@@ -4,36 +4,26 @@ const Registration = require('./Registration');
 
 User.hasMany(Event, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
 });
 
 Event.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-User.belongsTo(Event, {
-    through: Registration,
     foreignKey: 'user_id',
 });
 
-Event.belongsToMany(User, {
-    through: Registration,
-    foreignKey: 'event_id',
+User.hasMany(Registration, {
+    foreignKey: 'user_id',
 });
 
 Registration.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-Registration.belongsTo(Event, {
-    foreignKey: 'event_id',
-});
-
-User.hasMany(Registration, {
-    foreignKey: 'event_id',
-});
-
 Event.hasMany(Registration, {
+    foreignKey: 'event_id',
+});
+
+Registration.belongsTo(Event, {
     foreignKey: 'event_id',
 });
 
--- models/user.js
+++ models/user.js
@@ -11,44 +11,44 @@ class User extends Model {
 User.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true, 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [6],
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
       },
     },
-    {
-        hooks: {
-          async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            return newUserData;
-          },
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'User',
-      }
-    );
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'User',
+  }
+);
 
 module.exports = User;
