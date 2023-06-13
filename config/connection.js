d413cd2df7b90bbcf2965299013d62b91a655dd8
RandyYanish <rtaylorfargo@gmail.com>
2023-06-13 13:18:53 -0700

    connection update

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config(); 
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.CLEARDB_DATABSE_URL) {
+  // Use ClearDB on Heroku
+  sequelize = new Sequelize(process.env.CLEARDB_DATABSE_URL);
+} else {
+  // Use local MySQL
+  sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
-
