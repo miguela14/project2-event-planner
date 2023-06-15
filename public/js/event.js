178791713daca2db15e6d655341a792810e69371
RandyYanish <rtaylorfargo@gmail.com>
2023-06-14 19:29:57 -0700

    profile.handlebars and event.js

--- public/js/event.js
+++ public/js/event.js
@@ -4,14 +4,14 @@ const newFormHandler = async (event) => {
     const title = document.querySelector('#event-title').value.trim();
     const description = document.querySelector('#event-description').value.trim();
     const date = document.querySelector('#event-date').value.trim();
-    const time = document.querySelector('#event-time').value.trim();
-    const duration = document.querySelector('#event-duration').value.trim();
     const location = document.querySelector('#event-location').value.trim();
+    const startTime = document.querySelector('#start-time').value.trim();
+    const endTime = document.querySelector('#end-time').value.trim();
 
-    if (title && description && date && time && duration && location) {
+    if (title && description && date && location && startTime && endTime) {
         const response = await fetch('/api/events', {
             method: 'POST',
-            body: JSON.stringify({ title, description, date, time, duration, location }),
+            body: JSON.stringify({ title, description, date, location, startTime, endTime }),
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -24,3 +24,23 @@ const newFormHandler = async (event) => {
         }
     }
 };
+
+const deleteButtonHandler = async (event) => {
+    if (event.target.hasAttribute('event-id')) {
+        const id = event.target.getAttribute('event-id');
+
+        const response = await fetch(`/api/events/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete Event');
+        }
+    }
+};
+
+document.querySelector('#event-submit').addEventListener('click', newFormHandler);
+
+document.querySelector('.event-list').addEventListener('click', deleteButtonHandler);
